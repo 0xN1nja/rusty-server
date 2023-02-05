8f6af8d4b6c9c270760fc9fb95f398051d90f18e
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-02-05 17:58:45 +0530

    Created A Function To Handle Connections

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,7 @@ use std::env::{self, *};
 use std::io::prelude::{self, *};
 use std::net::{self, *};
 
-fn main() {
-    let port: u32 = std::env::args().nth(1).unwrap().parse::<u32>().unwrap();
-    let socket: std::net::TcpListener =
-        std::net::TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
-    println!("Server Listening On http://localhost:{}/", port);
+fn handle_connection(socket: std::net::TcpListener, port: u32) -> std::io::Result<()> {
     for i in socket.incoming() {
         let mut stream: std::net::TcpStream = match i {
             Ok(_stream) => _stream,
@@ -25,4 +21,13 @@ fn main() {
         stream.write(response.as_bytes());
         stream.flush();
     }
+    Ok(())
+}
+
+fn main() {
+    let port: u32 = std::env::args().nth(1).unwrap().parse::<u32>().unwrap();
+    let socket: std::net::TcpListener =
+        std::net::TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+    println!("Server Listening On http://localhost:{}/", port);
+    handle_connection(socket, port).unwrap();
 }
